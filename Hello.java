394afbe56ded0d2bf076be53787983a2b9ea9dcf
Mika Nokka <mika.nokka@ambientia.fi>
2015-08-13 13:20:10 +0300

    Hello initial commit

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+ *  compile javac Hello.java
+ *  execute: java HelloW
+ *
+ ******************************************************************************/
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+		int x=0;
+		while ( x<20 ) {
+			System.out.println("Hello, World "+x+"\n");
+			x++;
+			}
+	}
+
+}
