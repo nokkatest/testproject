973b17c2c4b131586b5cbf41e6df774a82f651b3
Mika <mika.nokka1@gmail.com>
2017-01-04 10:39:02 +0200

    Added Bamboo response parsing. OK / NOT OK build starting commanding

--- BambooCommander.py
+++ BambooCommander.py
@@ -6,17 +6,24 @@ import netrc
 import logging
 import sys, getopt, re
 import requests,netrc
-
+import logging
 
 def main(argv):
-    BambooSteps("https://bamboo.almdemo.fi","GITHUBINT-SGF")
+    BambooSteps("https://bamboo.almdemo.fi","GITHUBINT-SGF","DEBUG")
 
 ################################################################
 # server=Bamboo server
 # branch=Bamboo build plan to be build
 #
 
-def BambooSteps(server,theplan):
+def BambooSteps(server,theplan,debuglevel):
+
+
+    if (debuglevel=="DEBUG"):
+        loglevel = logging.DEBUG # Default logging level INFO, override with '-d' for logging.DEBUG
+    elif (debuglevel=="INFO"):
+        loglevel = logging.INFO # Default logging level INFO, override with '-d' for logging.DEBUG
+    logging.basicConfig(level=loglevel)
 
     #check password from .netrc
     # REMEMBER CHMOD 600 RIGHTS
@@ -41,8 +48,16 @@ def BambooSteps(server,theplan):
     
     URL="{0}/rest/api/latest/queue/{1}".format(server,theplan)
     #requests.post('https://bamboo.almdemo.fi/rest/api/latest/queue/GITHUBINT-SGF', data=data, auth=(user, password))
-    requests.post(URL, data=data, auth=(user, password))
+    r=requests.post(URL, data=data, auth=(user, password))
     #''http://bamboo.almdemo.fi/rest/api/latest/queue/GITHUBINT-SGF'
+    
+    logging.debug("Bamboo feedback:")
+    logging.debug(r.status_code)
+    logging.debug(r.headers)
+    if (r.status_code==requests.codes.ok):
+        logging.info("---> Bamboo build:{0} commanded OK to be built".format(theplan))
+    else:
+        logging.info("---> ERROR: Bamboo build:{0} commanding error:{1}".format(theplan,r.status_code))
 
 if __name__ == "__main__":
         main(sys.argv[1:])
