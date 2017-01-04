969e87aae0747a6c2988556dd42bdf3147a27261
Mika Nokka <mnokka@users.noreply.github.com>
2017-01-04 19:33:15 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -10,7 +10,7 @@ public class Hello {
     public static void main(String[] args) {
 		int x=0;
 		while ( x<20 ) {
-			System.out.println("Hello, World bbbb vvv "+x+"\n");
+			System.out.println("Hello, World bbbb vvv oo "+x+"\n");
 			x++;
 			}
 	}
